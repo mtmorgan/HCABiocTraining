9b99ab32e4eafd79cc58302a47a4fd8eacf1366c
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-11-05 14:22:28 -0400

    implement helper functions; incremental vignette expansion

---
+++ R/cxg_dataset.R
@@ -0,0 +1,93 @@
+#' @rdname cxg_dataset
+#'
+#' @title Summarize CxG Information About a Dataset
+#'
+#' @param dataset_id character(1) dataset identifier, as returned by,
+#'     e.g., `datasets()`.
+#'
+#' @importFrom dplyr filter
+#'
+#' @importFrom rjsoncons jmespath
+#'
+#' @importFrom jsonlite toJSON fromJSON
+#'
+#' @examples
+#' dataset <- "de985818-285f-4f59-9dbd-d74968fddba3"
+#' ds <- cxg_dataset(dataset)
+#' ds
+#'
+#' @export
+cxg_dataset <-
+    function(dataset_id, cellxgene_db = db())
+{
+    id <- dataset_id
+
+    ## retrieve dataset and collection information
+    dataset <-
+        datasets(cellxgene_db) |>
+        filter(.data$dataset_id == id)
+    collection <-
+        collections(cellxgene_db) |>
+        filter(.data$collection_id == dataset$collection_id)
+
+    ## create an object with relevant information
+    json <- jsonlite::toJSON(collection, auto_unbox = TRUE)
+    family <-
+        jmespath(json, "[].publisher_metadata[].authors[].family") |>
+        fromJSON()
+    given <- 
+        jmespath(json, "[].publisher_metadata[].authors[].given") |>
+        fromJSON()
+    authors <- paste(family, given, sep = ", ", collapse = "; ")
+    journal <-
+        jmespath(json, "[].publisher_metadata[].journal") |>
+        fromJSON()
+
+    dataset_assay <-
+        jmespath(dataset, "[].assay[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    dataset_organism <-
+        jmespath(dataset, "[].organism[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    dataset_ethnicity <-
+        jmespath(dataset, "[].self_reported_ethnicity[].label[]") |>
+        fromJSON() |>
+        paste(collapse = "; ")
+    structure(list(
+        collection_name = collection$name,
+        collection_description = collection$description,
+        collection_authors = authors,
+        collection_journal = journal,
+        dataset_assay = dataset_assay,
+        dataset_organism = dataset_organism,
+        dataset_ethnicity = dataset_ethnicity
+    ), class = "cxg_dataset")
+}
+
+
+.pretty <-
+    function(label, ..., indent = 0, exdent = 4)
+{
+    x <- paste(label, paste0(...), sep = ": ")
+    paste(strwrap(x, indent = indent, exdent = exdent), collapse = "\n")
+}
+
+#' @rdname cxg_dataset
+#'
+#' @export
+print.cxg_dataset <-
+    function(x, ...)
+{
+    cat(
+        .pretty("title", x$collection_name), "\n",
+        .pretty("description", x$collection_description), "\n",
+        .pretty("authors", x$collection_authors), "\n",
+        .pretty("journal", x$collection_journal), "\n",
+        .pretty("assays", x$dataset_assay), "\n",
+        .pretty("organism", x$dataset_organism), "\n",
+        .pretty("ethnicity", x$dataset_ethnicity), "\n",
+        sep = ""
+    )
+}
--- R/read_h5ad_as_sce.R
+++ R/read_h5ad_as_sce.R
@@ -1,3 +1,17 @@
+#' @rdname read_h5ad
+#'
+#' @title Read 'h5ad' Files as R / Bioconductor Objects
+#'
+#' @description `read_h5ad_as_sce()` uses the Python h5ad module and R
+#'     'anndata' package to import the `.h5ad` objects into R /
+#'     Bioconductor 'SingleCellExperiment'.
+#'
+#' @param h5ad_file_path `character(1)` path to the `.h5ad` file.
+#'
+#' @return `read_h5ad_as_sce()` returns an R / Bioconductor
+#'     SingleCellExperiment object. The assay data `X` is the (sparse)
+#'     matrix named 'X' in the h5ad file.
+#' 
 #' @importFrom anndata read_h5ad
 #'
 #' @importFrom SingleCellExperiment SingleCellExperiment
