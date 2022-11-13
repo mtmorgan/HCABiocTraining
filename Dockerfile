f4d4ff80df7f7009a7d2aeb94a53e9dab35b78f2
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-11-13 17:30:43 -0500

    update Dockerfile default package installations
    - fix? github link in navbar
    - no longer need to install stringi specially

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM bioconductor/bioconductor_docker:RELEASE_3_16
 
-COPY --chown=rstudio:rstudio . /home/rstudio/HCABiocTraining
+COPY --chown=rstudio:rstudio . /home/rstudio/
 
-RUN sh /home/rstudio/HCABiocTraining/docker/install_me.sh
-RUN Rscript /home/rstudio/HCABiocTraining/docker/install_me.R
-
+RUN sh /home/rstudio/docker/install_me.sh
 RUN echo "RETICULATE_PYTHON_ENV=/opt/venv/anndata" >> "${R_HOME}/etc/Renviron.site"
+
+RUN Rscript /home/rstudio/docker/install_me.R
+
--- docker/install_me.R
+++ docker/install_me.R
@@ -1,13 +1,10 @@
 BiocManager::install('reticulate')
-## reticulate::virtualenv_create(packages = 'anndata')
 
-## default installation seems to link to invalid libicui18n.so.66
-## install.packages('stringi', repos = BiocManager::repositories()[-1])
+BiocManager::install(ask=FALSE)
 
-## BiocManager::install(ask=FALSE)
-
-## devtools::install(
-##     '/home/rstudio',
-##     dependencies=TRUE,
-##     repos = BiocManager::repositories()
-## )
+devtools::install(
+    "/home/rstudio/",
+    dependencies=TRUE,
+    build_vignettes=TRUE,
+    repos = BiocManager::repositories()
+)
